b57c385c9cb279b7ee721f655b9027c52e65d6dc
zakirxx13 <zakirxx13@gmail.com>
2026-09-19 14:31:57 +0600

    Update info.js

--- functions/api/info.js
+++ functions/api/info.js
@@ -1,17 +1,25 @@
-export async function onRequest() {
+export async function onRequest(context) {
   return new Response(
-    JSON.stringify({
-      success: true,
-      name: "M3U8 Proxy API",
-      version: "1.0.0",
-      status: "online",
-      endpoint: "/api/proxy?url=ENCODED_M3U8_URL"
-    }, null, 2),
+    JSON.stringify(
+      {
+        success: true,
+        name: "M3U8 Proxy API",
+        version: "1.0.0",
+        status: "online",
+        message: "M3U8 Proxy API is running",
+        endpoint: "/api/proxy?url=YOUR_M3U8_URL"
+      },
+      null,
+      2
+    ),
     {
       status: 200,
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "*",
+        "Cache-Control": "no-store"
       }
     }
   );
